9d139d7a3b4a29c1d267345357137538bac62fd6
jorgeoscanoa <jorge.oscanoa@neoland.es>
2025-07-11 18:43:36 +0200

    Contact manger completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 
-import { ContactAdd } from './contacts/components/contact-add/contact-add';
+import { ContactList } from './contacts/components/contact-list/contact-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,ButtonModule,ContactAdd],
+  imports: [RouterOutlet,ButtonModule,ContactList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
